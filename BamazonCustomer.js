1d6c5a062b5f0eba339fdb5bba2f2e77bf13c199
Brent Woodcock <brentwoodcock@gmail.com>
2016-10-22 13:05:20 -0500

    added inquirer package and basic prompts

--- BamazonCustomer.js
+++ BamazonCustomer.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql');
+var inquirer = require('inquirer');
 
 var conn = mysql.createConnection({
     host: "localhost",
@@ -16,6 +17,30 @@ conn.connect(function(err) {
 
 	conn.query('SELECT * FROM products', function(err, res){
 		if(err) throw err;
-		console.log(res);
-	})
-})
+		console.log('ID   Product               Price($)');
+		console.log('---------------------------------------------');
+		var i = 0;
+		var products = [];
+		// Display products for sale
+		while (i < res.length){
+			console.log(res[i].ItemID + '    ' + res[i].ProductName + '    ' + res[i].Price);
+			products.push(res[i].ProductName);
+			i++;
+		}
+		// Prompt user for their product choice
+		inquirer.prompt([
+			{
+				type: 'list',
+				name: 'product',
+				message: 'Which product would you like to buy?',
+				choices: products
+			},{
+				type: 'input',
+				name: 'quantity',
+				message: 'How many would you like to purchase?'
+			}
+		]).then(function(answers) {
+			console.log(JSON.stringify(answers, null, '  '))
+		})
+	});
+});
